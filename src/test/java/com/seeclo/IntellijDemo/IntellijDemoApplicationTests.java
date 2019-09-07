5dfb559d8134e7f46fd083a28f9856e402f6a82b
Nitesh Kumar N <niteshkumarn55@gmail.com>
2019-09-07 14:39:11 +0530

    First commit

---
+++ src/test/java/com/seeclo/IntellijDemo/IntellijDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seeclo.IntellijDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntellijDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
